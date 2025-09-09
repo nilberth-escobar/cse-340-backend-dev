4d69b9b31c8b6d10edbb68f64f38e2304b8babfd
nilberth-escobar <nilberrhescobar@mail.com>
2025-09-08 22:12:28 -0600

    Home page layout working

--- server.js
+++ server.js
@@ -9,11 +9,26 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+/* ***********************
+ * Veiw engine and templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
+//index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
